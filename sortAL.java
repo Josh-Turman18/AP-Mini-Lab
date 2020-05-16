86f8076db724ddfd161655b988b56c4c16f96b68
Josh-Turman18 <42820729+Josh-Turman18@users.noreply.github.com>
2020-05-16 15:52:36 -0700

    started a class that will sort an ArrayList  -- "sortAL"

---
+++ sortAL.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sortAL {
+
+	public sortAL() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void selectionSort(ArrayList<Object> list)
+	{
+		
+	}
+
+}
