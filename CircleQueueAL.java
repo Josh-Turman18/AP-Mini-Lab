6b0e10cce2cd81ad1c0ce3c3f348a806e74d43c5
Josh-Turman18 <42820729+Josh-Turman18@users.noreply.github.com>
2020-05-16 14:58:15 -0700

    started work on the CircleQueueAL class

---
+++ CircleQueueAL.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CircleQueueAL {
+
+	// array list that will hold the objects in order
+	public ArrayList<Object> list;
+	
+	/*
+	 * constructor
+	 */
+	public CircleQueueAL() {
+		list = null;
+	}
+
+}
