d50e7a6395b2d1a4188f369571a5fccaa3cc41ad
Josh-Turman18 <42820729+Josh-Turman18@users.noreply.github.com>
2020-05-16 15:18:07 -0700

    added method to CircleQueueAL

--- CircleQueueAL.java
+++ CircleQueueAL.java
@@ -4,12 +4,48 @@ public class CircleQueueAL {
 
 	// array list that will hold the objects in order
 	public ArrayList<Object> list;
+	public int count;
 	
 	/*
 	 * constructor
 	 */
 	public CircleQueueAL() {
 		list = null;
+		count = 0;
+	}
+	
+	// adds an object to the ArrayList
+	public void addElement(Object[] objects)
+	{
+		int i = 0;
+		ConsoleMethods.println("Add " + objects.length);
+		for (Object o : objects)
+		{
+			list.add(o);
+			System.out.println("Add: " + objects[i++]);
+			this.count++;
+		}
+		System.out.println();
+	}
+	
+	// Displays the whole ArrayList
+	public void showAList()
+	{
+		System.out.println("Size: " + count);
+		System.out.println("First Element: " + list.get(0));
+		System.out.println("Last Element: " + list.get((this.count) - 1));
+		System.out.println("Full ArrayList: " + list);
+	}
+	
+	// deletes all the elements for the ArrayList
+	public void deleteList()
+	{
+		while(list != null)
+		{
+			System.out.print("Delete " + list.get(0) + " : ");
+			list.remove(0);
+			System.out.println(list);
+		}
 	}
 
 }
